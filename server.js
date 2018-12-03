d8d3b6a539b57ad3affc53b2b76f852d5d02f146
Alexander Schwartzberg <aeksco@gmail.com>
2018-12-03 17:03:48 -0500

    Added embed page, blank index.html page

--- server.js
+++ server.js
@@ -27,15 +27,20 @@ app.use(bodyParser.json());
 // Serve static client assets
 app.use(express.static('client'));
 
+// Home page
+app.get('/', function(req, res) { res.render('index'); });
+
+// Embed page
 // TODO - handle missing req.query
-app.get('/', function(req, res) {
+app.get('/embed', function(req, res) {
     // const myUrl = 'https://raw.githubusercontent.com/FRMA-Ontology/diagrams/master/concept-maps/oe_12/svg/OE_X_ImageOntology-full.svg'
     request.get(req.query.svgUrl, (err, resp) => {
       const body = resp.body.replace('<svg ', '<svg id="embeddy-svg" style="width:100%;" ')
-      res.render('index', { svgXml: body });
+      res.render('embed', { svgXml: body });
     })
 });
 
+// Test page
 app.get('/test', function(req, res) {
   res.render('about');
 });
