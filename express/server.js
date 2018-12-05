a2f0711a476b00731bf7d7b0fd5ae2fb4167c43c
Alexander Schwartzberg <aeksco@gmail.com>
2018-12-05 11:23:02 -0500

    Directory cleanup, migrating old express code to netlify function

--- express/server.js
+++ express/server.js
@@ -1,8 +1,27 @@
 'use strict';
 const express = require('express');
 const serverless = require('serverless-http');
-const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+// // // //
+
+// Express.js App & Configuration
+const app = express();
+
+// Cors configuration
+app.use(cors());
+
+// App middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Sets up static serving of files
+app.use(express.static('public'));
+
+// Sets EJS as the view engine
+// app.set('view engine', 'ejs');
 
 const router = express.Router();
 
@@ -15,7 +34,6 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const serverless = require('serverless-http');
-
-// // // //
-
-module.exports.run = serverless(app)
